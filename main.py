5aa1d78c05c882809750199ea4c2bf3ca0a38a48
mthiruranganadh <mthiruranganadh09@gmail.com>
2026-08-03 19:23:06 +0530

    update

--- main.py
+++ main.py
@@ -21,8 +21,11 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "http://localhost:3001",
         "http://127.0.0.1:3000",
+        "http://127.0.0.1:3001",
         "https://opportunitymatch-frontend-mthiruranganadh.vercel.app/",
+        "https://opportunitymatch-frontend-mthiruranganadh.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
